2db2b583844c599e4e91a0b1cc1f09dda9a27734
bulochkka <ram23zan@mail.ru>
2024-02-05 20:29:03 +0300

    Commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Anime", "Naruto", (byte) 23);
+        us.saveUser("Stas", "Mihailov", (byte) 99);
+        us.saveUser("Jafar", "Harisa", (byte) 20);
+        us.saveUser("asdads", "asfafdf", (byte) 55);
+        List<User> list = us.getAllUsers();
+        list.forEach(System.out::println);
+        us.cleanUsersTable();
+        us.dropUsersTable();
     }
 }
